0023c04fd6deb82842ae7082188bd56867833e1e
todoggler <kentokuraCS@gmail.com>
2023-12-09 21:22:14 +0900

    サーバーにアクセスするためのURL出力を app.js に移動
    参考: https://developer.mozilla.org/ja/docs/Learn/Server-side/Express_Nodejs/Introduction

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //expressモジュールを使えるように設定
 const express = require('express')
+
+//ホスト名とポート番号を指定
+const hostname = '127.0.0.1';
+const port = 8000;
+
 //expressモジュールを利用しアプリケーションオブジェクトappを作成
 const app = express()
 
@@ -9,4 +14,6 @@ app.get('/', (req, res) => {
 })
 
 //サーバーを起動したら、リクエストを8000番ポートで待ち受ける設定。
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
